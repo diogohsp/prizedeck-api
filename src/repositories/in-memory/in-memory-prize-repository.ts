7acba5959f387cdbc3f9255b97ebc3c354d08a4c
diogohsp <ggdiogogg@gmail.com>
2024-12-11 07:54:20 -0300

    feat: add data prize model and in memory prize database for test

---
+++ src/repositories/in-memory/in-memory-prize-repository.ts
@@ -0,0 +1,39 @@
+import { Prize, Prisma } from "@prisma/client";
+import { PrizesRepository } from "../prizes-repository";
+
+export class InMemoryPrizeRepository implements PrizesRepository {
+
+    public items: Prize[] = []
+
+    async findById(id: string): Promise<Prize | null> {
+        const prize = this.items.find(item => item.id === id)
+
+        if(!prize){
+            return null
+        }
+
+        return prize
+    }
+    async findByName(name: string): Promise<Prize | null> {
+        const prize = this.items.find(item => item.name === name)
+
+        if(!prize){
+            return null
+        }
+
+        return prize
+    }
+    async registerPrize(data: Prisma.PrizeCreateInput): Promise<Prize> {
+
+        const prize = {
+            id: 'prize-1',
+            name: data.name,
+            quantity: data.quantity
+        }
+
+        this.items.push(prize)
+
+        return prize
+    }
+
+}
