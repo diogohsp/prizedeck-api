0645dc4b0a3c052b2dfe88c88b021c329c6a5c71
hspDiogo <diogo.prado@gruponsc.com.br>
2024-12-13 15:43:32 -0300

    feat: making factories for services and refactoring tests by adding instance of repository and service beforeEach test

--- src/services/registerDaterPrize/registerDatePrize.spec.ts
+++ src/services/registerDaterPrize/registerDatePrize.spec.ts
@@ -1,13 +1,21 @@
 import { describe, expect, it } from "vitest";
 import { RegisterDatePrizeService } from "./registerDatePrize";
 import { InMemoryDatePrizeRepository } from "@/repositories/in-memory/in-memory-dateprize-repository";
+import { beforeEach } from "node:test";
 
 describe.only('Register Date Prize', () => {
-    it('should be possible register a date prize', async  () => {
-        const inMemoryDatabase = new InMemoryDatePrizeRepository
-        const registerDatePrizeService = new RegisterDatePrizeService(inMemoryDatabase)
 
-        const { datePrize } = await registerDatePrizeService.execute({
+    let datePrizeRepository: InMemoryDatePrizeRepository
+    let sut: RegisterDatePrizeService
+
+    beforeEach(() => {
+        datePrizeRepository = new InMemoryDatePrizeRepository
+        sut = new RegisterDatePrizeService(datePrizeRepository)
+    })
+
+    it('should be possible register a date prize', async  () => {
+
+        const { datePrize } = await sut.execute({
             dateHourPrize: new Date(),
             prize: {
                 code: '01'
