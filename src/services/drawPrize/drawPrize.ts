900e2a683dbdb197d230a2db71ef1af9dfdf4952
diogohsp <ggdiogogg@gmail.com>
2025-02-17 18:43:00 -0300

    feat: base draw prize

---
+++ src/services/drawPrize/drawPrize.ts
@@ -0,0 +1,35 @@
+import { PrizesRepository } from "@/repositories/prizes-repository"
+import { DatePrize, Prize } from "@prisma/client"
+import { DatePrizeRepository } from "@/repositories/dateprizes-repository"
+import { DatePrizeAlreadyAwardedError } from "../errors/prize-already-awarded"
+
+interface WinPrizeParams{
+    prize_id: string
+}
+
+interface WinPrizeResponse{
+    datePrize: DatePrize | null
+}
+
+export class WinPrizeService{
+
+    constructor(private datePrizesRepository: DatePrizeRepository){}
+
+    async execute({ prize_id }: WinPrizeParams): Promise<WinPrizeResponse> {
+        
+        const prizesParaPremiar = await this.datePrizesRepository.findAll
+
+        const prizeAwarded = await this.datePrizesRepository.findPrizeAwarded(prize_id)
+         
+        if(prizeAwarded){
+            throw new DatePrizeAlreadyAwardedError
+        }
+    
+        const datePrize = await this.datePrizesRepository.winPrize(prize_id) 
+
+        return {
+            datePrize
+        }
+    }
+
+}
